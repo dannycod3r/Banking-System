4cb318f1232c3b7cf924dc9b399c451fe01ac214
Gabriel Tengey <gabrieltengey123@gmail.com>
2023-03-16 23:29:59 +0000

    static database for testing authentication

---
+++ user_database.py
@@ -0,0 +1,42 @@
+database = {
+    "Jenica": {
+        "id": 4,
+        "first_name": "Jenica",
+        "last_name": "Kell",
+        "email": "jkell3@nymag.com",
+        "password": "xQjyFxf",
+        "balance": 0.0
+        },
+    "Moll": {
+        "id": 5,
+        "first_name": "Moll",
+        "last_name": "Absolon",
+        "email": "mabsolon4@ebay.co.uk",
+        "password": "0SxJhLApa5f",
+        "balance": 0.0
+    },
+    'Obidiah': {
+        "id": 6,
+        "first_name": "Obidiah",
+        "last_name": "Herculson",
+        "email": "oherculson5@virginia.edu",
+        "password": "qyNiERz6I",
+        "balance": 0.0
+    },
+    'Sherie': {
+        "id": 7,
+        "first_name": "Sherie",
+        "last_name": "Tofano",
+        "email": "stofano6@cbc.ca",
+        "password": "KyRu6q9lja",
+        "balance": 0.0
+    },
+}
+
+
+def user(username):
+    return username
+
+
+if __name__ == '__main__':
+    print(database['Sherie']['last_name'])
